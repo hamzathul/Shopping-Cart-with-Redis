fa30c7ed487354b69a16a33a7b0d38fa2f9dfab0
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-10-26 08:29:09 +0530

    ongoing:signup controller

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import bcrypt from 'bcryptjs'
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -44,22 +44,22 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema);
-
 // Pre-save hook to hash password before saving to the database
-userSchema.pre("save", async function(next){
-    if(!this.isModified("password")) return next()
-    try {
-        const salt = await bcrypt.genSalt(10)
-        this.password = await bcrypt.hash(this.password, salt)
-        next()
-    } catch (error) {
-        next(error)
-    }
-})
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
-userSchema.methods.comparePassword = async function(password){
-    return bcrypt.compare(password, this.password)
-}
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
 
 export default User;
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,20 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
 
-import authRoutes from './routes/auth.route.js'
-import { connectDB } from './lib/db.js';
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.use('/api/auth', authRoutes)
+app.use(express.json());
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on PORT : ${PORT}`)
-    connectDB()
-})
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT : ${PORT}`);
+  connectDB();
+});
