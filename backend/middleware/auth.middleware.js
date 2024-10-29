ace1cfff394c7a1d1edaa65730ec23aedaf82d45
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-10-29 08:06:06 +0530

    middleware: Admin Route

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -36,3 +36,11 @@ export const protectRoute = async (req, res, next) => {
       .json({ message: "Unauthorized - Invalid access token" });
   }
 };
+
+export const adminRoute = async (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    return res.status(403).json({ message: "Access denied - Admin Only" });
+  }
+};
