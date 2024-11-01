7c09efc5269d543c1bb510ed0f30d151b558979c
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-11-01 09:13:18 +0530

    removeAllFromCart controller

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,12 +1,15 @@
-import express from 'express'
-import { addToCart } from '../controllers/cart.controller.js'
-import { protectRoute } from '../middleware/auth.middleware.js'
+import express from "express";
+import {
+  addToCart,
+  removeAllFromCart,
+} from "../controllers/cart.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
-const router = express.Router()
+const router = express.Router();
 
 // router.get('/', protectRoute, getCartProducts)
-router.post('/', protectRoute, addToCart)
-// router.delete('/', protectRoute, removeAllFromCart)
+router.post("/", protectRoute, addToCart);
+router.delete("/", protectRoute, removeAllFromCart);
 // router.put('/:id', protectRoute, updateQuantity)
 
-export default router
+export default router;
