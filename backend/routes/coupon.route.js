a220236387e6897a159e2b5d6b2c4f62d6b8189e
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-11-01 11:12:02 +0530

    validateCoupon controller

--- backend/routes/coupon.route.js
+++ backend/routes/coupon.route.js
@@ -1,9 +1,10 @@
-import express from 'express'
-import { protectRoute } from '../middleware/auth.middleware.js'
-import { getCoupon } from '../controllers/coupon.controller.js'
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getCoupon, validateCoupon } from "../controllers/coupon.controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', protectRoute, getCoupon)
+router.get("/", protectRoute, getCoupon);
+router.get("/validate", protectRoute, validateCoupon);
 
-export default router
+export default router;
