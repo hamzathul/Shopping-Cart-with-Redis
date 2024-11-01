1525d8b57c240b04b91840a7395a652d586c44ff
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-11-01 10:13:38 +0530

    getCartProducts controller

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   addToCart,
+  getCartProducts,
   removeAllFromCart,
   updateQuantity,
 } from "../controllers/cart.controller.js";
@@ -8,9 +9,9 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-// router.get('/', protectRoute, getCartProducts)
+router.get("/", protectRoute, getCartProducts);
 router.post("/", protectRoute, addToCart);
 router.delete("/", protectRoute, removeAllFromCart);
-router.put('/:id', protectRoute, updateQuantity)
+router.put("/:id", protectRoute, updateQuantity);
 
 export default router;
