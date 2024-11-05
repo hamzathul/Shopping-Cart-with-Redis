5cb67c97695017223690f7d7eaacc63f8974a340
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-11-05 09:44:08 +0530

    analytics controller completed

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -28,3 +28,56 @@ export const getAnalyticsData = async () => {
     totalRevenue,
   };
 };
+
+export const getDailySalesData = async (startDate, endDate) => {
+  try {
+    const dailySalesData = await Order.aggregate([
+      {
+        $match: {
+          createdAt: {
+            $gte: startDate,
+            $lte: endDate,
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+          sales: { $sum: 1 },
+          revenue: { $sum: "$totalAmount" },
+        },
+      },
+      {
+        $sort: {
+          _id: 1,
+        },
+      },
+    ]);
+
+    const dateArray = getDatesInRange(startDate, endDate);
+
+    return dateArray.map((date) => {
+      const foundData = dailySalesData.find((item) => item._id === date);
+
+      return {
+        date,
+        sales: foundData?.sales || 0,
+        revenue: foundData?.revenue || 0,
+      };
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+function getDatesInRange(startDate, endDate) {
+  const dates = [];
+  let currentDate = new Date(startDate);
+
+  while (currentDate <= endDate) {
+    dates.push(currentDate.toISOString().split("T")[0]);
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return dates;
+}
