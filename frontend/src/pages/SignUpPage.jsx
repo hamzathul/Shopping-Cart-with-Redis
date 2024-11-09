6181ca4e5c3ee79204c37905b8de75469e7eae3e
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-11-09 16:25:36 +0530

    signup and state mgmt using zustand

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 import { motion } from "framer-motion";
+import { useUserStore } from "../stores/useUserStore";
 
 const SignUpPage = () => {
   const loading = false;
@@ -12,9 +13,10 @@ const SignUpPage = () => {
     confirmPassword: "",
   });
 
+  const { signup } = useUserStore();
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
+    signup(formData);
   };
   return (
     <div className="flex flex-col justify-center sm:px-6 lg:px-8">
