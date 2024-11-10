bd0b11530b1593510533a4c363665bb0c9088043
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-11-10 10:10:45 +0530

    cors setup

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
-import analyticsRoutes from './routes/analytics.route.js'
+import analyticsRoutes from "./routes/analytics.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -15,6 +17,10 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+//remove it from production
+app.use(morgan("dev"));
 
 app.use(express.json());
 app.use(cookieParser());
