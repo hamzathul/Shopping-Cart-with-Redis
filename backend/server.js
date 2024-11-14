69769bed58f2d31751e111a8ddaa8a919d63eeed
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-11-14 08:35:13 +0530

    set json limit for uploads

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
 //remove it from production
 app.use(morgan("dev"));
 
-app.use(express.json());
+app.use(express.json({limit:"10mb"}));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
