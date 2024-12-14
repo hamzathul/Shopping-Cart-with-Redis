d02dbf071d4db6c4c49695cc81e514928da2b7d2
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-12-14 10:52:26 +0530

    fix:addToCart from  FeaturedProducts

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL:
-    import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
-  // baseURL: "http://localhost:5000/api",
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "/api",
   withCredentials: true,
 });
 
